81e53f6d38fe9acc536088ff1da06e0a7cbbacf1
N5NMR <131434371+N5NMR@users.noreply.github.com>
2025-02-21 16:49:06 +0100

    Add files via upload

--- UtilityPrg_16x2_I2CDisplay.ino
+++ UtilityPrg_16x2_I2CDisplay.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// Erstelle ein LCD-Objekt: Adresse 0x27, 20 Zeichen, 4 Zeilen
+// Erstelle ein LCD-Objekt: Adresse 0x27, 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 //LiquidCrystal_I2C lcd(0x27, 20, 4);
 
@@ -12,7 +12,7 @@ void setup() {
 
   // Begrüßungsnachricht anzeigen
   lcd.setCursor(0, 0); // Cursor auf die erste Zeile setzen
-  lcd.print("Wemos D1 Mini Pro");
+  lcd.print("Wemos D1 Mini");
   lcd.setCursor(0, 1); // Cursor auf die zweite Zeile setzen
   lcd.print("LCD Test Program");
 
@@ -26,7 +26,7 @@ void setup() {
   lcd.setCursor(0, 0);
   lcd.print("I2C Address: 0x27");
   lcd.setCursor(0, 1);
-  lcd.print("20x4 LCD Display");
+  lcd.print("LCD Display");
   lcd.setCursor(0, 2);
   lcd.print("Have Fun!");
 }
